1138d7de3ede7061e265d89cbb59faf0524afe0c
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-04-05 15:32:46 +0100

    updated for new form in form wizard

--- messaging/urls.py
+++ messaging/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from messaging.views import (SendSmsWizard, skip_if_single_receipient,
 skip_if_bulk_receipient, initial_data_for_wizard, SavedDraftListView,
-VerifyView, ResendSuccessView, NumberListView, HomePageView)
+VerifyView, ResendSuccessView, NumberListView, HomePageView, is_file_load)
 
-from messaging.forms import MessageForm, SingleReceipientForm, BulkReceipientForm
+from messaging.forms import MessageForm, SingleReceipientForm, BulkReceipientForm, FileUploadForm
 
-compose_forms = [MessageForm, SingleReceipientForm, BulkReceipientForm]
+compose_forms = [MessageForm, SingleReceipientForm, BulkReceipientForm, FileUploadForm]
 
 
 
@@ -14,11 +14,13 @@ urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view(), name='home'),
     
     url(r'^send/$', SendSmsWizard.as_view(compose_forms,
-        condition_dict={'1': skip_if_single_receipient, '2': skip_if_bulk_receipient}),
+        condition_dict={'1': skip_if_single_receipient, '2': skip_if_bulk_receipient,
+                        '3':is_file_load}),
         name='send'),
 
     url(r'^send/(?P<msg_id>\d+)/$', SendSmsWizard.as_view(compose_forms,
-        condition_dict={'1': skip_if_single_receipient, '2': skip_if_bulk_receipient},
+        condition_dict={'1': skip_if_single_receipient, '2': skip_if_bulk_receipient,
+                        '3': is_file_load},
         initial_dict={'0':initial_data_for_wizard}), name='send_to_others'),
 
     url(r'^sent/$', SavedDraftListView.as_view(), name='all_sent'),
