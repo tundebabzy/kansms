487fb342829c9f628ba36768cda98e42cc560367
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-04-13 23:58:06 +0100

    added ReaadOnly widget to display field as read-only i.e in p tags

---
+++ widgets/widget.py
@@ -0,0 +1,9 @@
+from django.forms.widgets import Widget
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+class ReadOnly(Widget):
+    """Some of these values are read only - just a bit of text..."""
+    def render(self, name, value, attrs=None):
+        value = force_unicode(value)
+        return mark_safe('<p>%s</p>' %value)
