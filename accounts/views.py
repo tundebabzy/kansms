7c40dfd0de42e02f95fb93d289a8bed2c3771604
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-04-13 12:51:54 +0100

    refactored account_view.profile_edit

--- accounts/views.py
+++ accounts/views.py
@@ -1,94 +1,14 @@
 from accounts import forms
-
-from userena.decorators import secure_required
-from userena.utils import get_profile_model, get_user_model
-from userena import settings as userena_settings
-from userena.views import ExtraContextTemplateView
-
-from django.shortcuts import redirect, get_object_or_404
-from django.contrib import messages
-from django.utils.translation import ugettext as _
-from django.core.urlresolvers import reverse
+from userena import views as userena_views
 from django.http import Http404
 
-from guardian.decorators import permission_required_or_403
-
-@secure_required
-@permission_required_or_403('change_profile', (get_profile_model(), 'user__username', 'username'))
 def profile_edit(request, username, edit_profile_form=forms.KansmsEditProfileForm,
                  template_name='userena/profile_form.html', success_url=None,
                  extra_context=None, **kwargs):
-    """
-    Edit profile.
-
-    Edits a profile selected by the supplied username. First checks
-    permissions if the user is allowed to edit this profile, if denied will
-    show a 404. When the profile is successfully edited will redirect to
-    ``success_url``.
-
-    :param username:
-        Username of the user which profile should be edited.
-
-    :param edit_profile_form:
-
-        Form that is used to edit the profile. The :func:`EditProfileForm.save`
-        method of this form will be called when the form
-        :func:`EditProfileForm.is_valid`.  Defaults to :class:`EditProfileForm`
-        from userena.
-
-    :param template_name:
-        String of the template that is used to render this view. Defaults to
-        ``userena/edit_profile_form.html``.
-
-    :param success_url:
-        Named URL which will be passed on to a django ``reverse`` function after
-        the form is successfully saved. Defaults to the ``userena_detail`` url.
-
-    :param extra_context:
-        Dictionary containing variables that are passed on to the
-        ``template_name`` template.  ``form`` key will always be the form used
-        to edit the profile, and the ``profile`` key is always the edited
-        profile.
-
-    **Context**
-
-    ``form``
-        Form that is used to alter the profile.
-
-    ``profile``
-        Instance of the ``Profile`` that is edited.
-
-    """
-    user = get_object_or_404(get_user_model(),
-                             username__iexact=username)
-
-    profile = user.get_profile()
-
-    user_initial = {'first_name': user.first_name,
-                    'last_name': user.last_name}
-
-    form = edit_profile_form(instance=profile, initial=user_initial)
-
-    if request.method == 'POST':
-        form = edit_profile_form(request.POST, request.FILES, instance=profile,
-                                 initial=user_initial)
-
-        if form.is_valid():
-            profile = form.save()
-
-            if userena_settings.USERENA_USE_MESSAGES:
-                messages.success(request, _('Your profile has been updated.'),
-                                 fail_silently=True)
-
-            if success_url: redirect_to = success_url
-            else: redirect_to = reverse('userena_profile_detail', kwargs={'username': username})
-            return redirect(redirect_to)
-
-    if not extra_context: extra_context = dict()
-    extra_context['form'] = form
-    extra_context['profile'] = profile
-    return ExtraContextTemplateView.as_view(template_name=template_name,
-                                            extra_context=extra_context)(request)
+    
+    return userena_views.profile_edit(request=request, username=username,
+            edit_profile_form=edit_profile_form, template_name=template_name,
+            success_url=success_url, extra_context=extra_context)
 
 def not_allowed(request):
     raise Http404
